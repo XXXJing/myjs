16c9638b993466b2aa046b44a55f20cd9e2db8b6
XJ <xiejinglink@163.com>
2020-09-24 11:25:39 +0800

    添加一些笔记

--- leetcode/numnerReverse/index.js
+++ leetcode/numnerReverse/index.js
@@ -15,5 +15,10 @@ var reverse = function(x) {
     if(result > maxLimit || result < minLimit) result = 0;
     return result;
 }
-
+/* 
+toString(radix)
+    radix 可选。规定表示数字的基数，使 2 ~ 36 之间的整数。
+    若省略该参数，则使用基数 10。但是要注意，如果该参数是 10 以外的其他值，则 ECMAScript 标准允许实现返回任意值。
+*/
 console.log(reverse(1534236469))
+
